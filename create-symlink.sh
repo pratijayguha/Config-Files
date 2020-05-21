2e2d3626ac8eadc11ec76b6beac762bbb0d7953e
pratijayguha <pratijayguha1@gmail.com>
2020-05-21 18:23:57 +0530

    This is the first commit

---
+++ create-symlink.sh
@@ -0,0 +1,32 @@
+# #!/bin/bash/
+
+CONFIG_DIR='$HOME/Config-Files'
+
+# Symlinks for config files
+ln -s $CONFIG_DIR/.LS_COLORS $HOME/
+ln -s $CONFIG_DIR/.conkyrc $HOME/
+ln -s $CONFIG_DIR/.zshrc $HOME/
+ln -s $CONFIG_DIR/.tmux.conf $HOME/
+ln -s $CONFIG_DIR/.gmshrc $HOME/
+
+# OhMyZSH Themes
+ln -s $CONFIG_DIR/agnoster-minimal.zsh-theme $ZSH/themes/agnoster-minimal.zsh-theme
+ln -s $CONFIG_DIR/agnoster-mod.zsh-theme $ZSH/themes/agnoster-mod.zsh-theme
+ln -s $CONFIG_DIR/zsh-syntax-highlighting $ZSH/plugins/zsh-syntax-highlighting
+ln -s $CONFIG_DIR/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
+
+# To run sudo commands when not using root
+SUDO=""
+if [ "$(whoami)" = "root" ] ; then
+	echo -e "You are the almighty root!"
+else
+	echo -e "You are just an ordinary user \n\n****ROOT AUTHORIZATION NEEDED****\n"
+	SUDO="sudo"
+fi
+
+# Symlinks for Themes
+$SUDO ln -s $CONFIG_DIR/Adapta-Orange-Nokto-Eta /usr/share/themes
+$SUDO ln -s $CONFIG_DIR/Adapta-Dots /usr/share/themes
+
+
+
